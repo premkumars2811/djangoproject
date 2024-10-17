34e4f37ce6b852dfe59fb55f2db84850fe8cb2c2
premkumars2811 <167157852+premkumars2811@users.noreply.github.com>
2024-10-16 21:07:38 -0800

    added host

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0$6k^f+#9uk(e44freh&ny-@%jryl0n(culsft4c9jbt)s1+-d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
